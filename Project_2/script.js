302b511bc98ab46557f584a66da48482d046f45f
sarfarazstark <sarfarazstark@gmail.com>
2024-03-24 06:12:13 +0530

    Previous Page animation done

--- Project_2/script.js
+++ Project_2/script.js
@@ -2,20 +2,29 @@
 const btn = document.querySelector('button');
 let activeBtns = 1;
 
-const steps = function (p, s, l) {
+const stepsUp = function (p, s, l) {
 	activeBtns++;
 	for (let i = 1; i < p; i++) {
 		const transitionTime =
 			activeBtns === p && i === p - 1
-				? 'all 0.1s ease-in-out'
-				: `all 0.3s ease-in-out 0.${3 * i}s`;
+				? 'all 0.3s ease-in-out'
+				: `all 0.3s ease-in-out ${0.3 * i}s`;
 		l[i - 1].style.transition = transitionTime;
 		s[i].style.transition = transitionTime;
 		l[i - 1].style.width = '100%';
 		s[i].classList.add('active');
 	}
 };
-
+const stepsDown = function (p, s, l, a) {
+	let increaseDelay = 0;
+	for (let i = a; i > p; i--) {
+		s[i - 1].style.transition = `all 0.3s ease-in-out ${0.3 * increaseDelay}s`;
+		l[i - 2].style.transition = `all 0.3s ease-in-out ${0.3 * increaseDelay}s`;
+		s[i - 1].classList.remove('active');
+		l[i - 2].style.width = '0';
+		increaseDelay++;
+	}
+};
 btn.parentElement.addEventListener('click', function (e) {
 	if (e.target.localName === 'button') {
 		const parentElement = e.target.parentElement;
@@ -26,13 +35,9 @@ btn.parentElement.addEventListener('click', function (e) {
 			parentElement.querySelectorAll('button.active').length;
 
 		if (pageNumbers < activeSiblings) {
-			for (let i = pageNumbers; i < activeSiblings; i++) {
-				siblings[i].style.transition = 'all 0.3s ease-in-out';
-				siblings[i].classList.remove('active');
-				line[i - 1].style.transition = 'all 0.3s ease-in-out';
-				line[i - 1].style.width = '0';
-			}
+			stepsDown(pageNumbers, siblings, line, activeSiblings);
 		}
-		steps(pageNumbers, siblings, line);
+
+		stepsUp(pageNumbers, siblings, line);
 	}
 });
