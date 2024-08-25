f7b1cc25f35324cd9f1f1537c13a5178952857f5
Md Sarfaraz Alam <sarfarazstark@gmail.com>
2024-08-25 23:27:55 +0530

    8th Form Wave Animations Completed ✅

--- 8-Form Wave Animation/script.js
+++ 8-Form Wave Animation/script.js
@@ -5,7 +5,9 @@ labels.forEach((label) => {
 		.split('')
 		.map(
 			(letter, index) =>
-				`<span style="transition-delay:${index * 50}ms">${letter}</span>`
+				`<span style="transition-delay:${index * 50}ms">${
+					letter === ' ' ? '\xa0' : letter
+				}</span>`
 		)
 		.join('');
 });
