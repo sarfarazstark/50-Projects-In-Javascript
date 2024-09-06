88d608aa934d37059d223a98425cf8a73cff8214
Md Sarfaraz Alam <sarfarazstark@gmail.com>
2024-09-06 21:25:23 +0530

    chore: Update script.js for table loading simulation and anchor icon insertion

---
+++ script.js
@@ -0,0 +1,36 @@
+// Simulate table loading
+window.addEventListener('load', () => {
+	const spinner = document.getElementById('spinner');
+	const content = document.getElementById('content');
+	setTimeout(() => {
+		spinner.style.display = 'none'; // Hide the spinner
+		content.style.display = 'flex'; // Show the content
+	}, 1500); // Adjust the delay as needed
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	document.querySelectorAll('tbody > tr').forEach((tr) => {
+		const status = tr.querySelector('td:nth-child(2)');
+		const problemAnchor = tr.querySelector('td:nth-child(4) > a');
+		const solutionAnchor = tr.querySelector('td:nth-child(5) > a');
+
+		if (problemAnchor) {
+			problemAnchor.innerHTML = `${problemAnchor.innerHTML}<i data-feather="help-circle"></i> `;
+		}
+
+		if (solutionAnchor) {
+		}
+
+		if (status && status.textContent.trim() === '❌') {
+			if (solutionAnchor) {
+				solutionAnchor.href = '#';
+				solutionAnchor.style.color = 'gray';
+				solutionAnchor.style.cursor = 'not-allowed';
+			}
+		} else {
+			solutionAnchor.innerHTML = `${solutionAnchor.innerHTML}<i data-feather="check-circle"></i>`;
+		}
+	});
+
+	feather.replace();
+});
